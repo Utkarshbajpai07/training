19b9e29bed74631f5b6a7a8e591ce8a741f132ba
Utkarshbajpai07 <utkarshb928@gmail.com>
2024-08-16 10:40:59 +0530

    changes in web

---
+++ nodejs/index.js
@@ -0,0 +1,84 @@
+console.log("hellon utkarsh")
+
+// alert("Hello TR-4")
+
+//var let const
+
+
+// datatype-- Primitive Types , reference type Data Type
+
+// Primitive Type - string , number , boolran , undefined , symbol,bigInt
+//Reference Type -array , object , date , null
+
+// var a ="Utkarsh Bajpai"
+// console.log(a)
+
+// var a=20;
+// console.log(a);
+
+// var a = true;
+// console.log(a);
+
+// var a = undefined;
+// console.log(typeof a);
+
+// var a;
+// console.log(a);
+
+// var a=[10,20,true,"Utkarsh"];
+// console.log(a);
+
+// var a ={
+//     firstName: "Utkarsh",
+//     lastName:"Bajpai"
+// }
+// console.log(a);
+
+//[{},{},{}]-- json--js object notation
+//{[]}
+// var date = new date();
+// console.log(typeof date);
+// var a = null;
+// console.log(typeof a);
+
+//Ecma script
+//2015------ES06
+// var b = 20;
+// var b = 40;
+
+// varibal with var can be redecleared or can be re assigned
+// let c = 100;
+// c = 200;
+// variable with let cannot be redecleared but can be re assigned
+// const d =1000;
+//d=2000;
+// variable with const cannot be redecleared
+
+// let a=10;
+// let b=20;
+// let sum=a+b;
+// let sub =a-b;
+// let mul=a*b;
+// let div=a/b;
+// let rem =a%b;
+// let exp = a**b;
+// console.log(sum,sub,mul,div,rem,exp);
+
+//Comparision Operator
+// let num1= 50;
+// let num2= "50"
+
+// console.log(num1==num2);
+// console.log(num1===num2);
+// console.log(num1=== +num2);// using + is known as type coersion
+
+// console.log(num1!=num2);
+// console.log(num1!==num2);
+// console.log(5/'5');
+
+// logic operators
+let isLoggedIn = 10;
+let isAdmin=20;
+
+console.log(isLoggedIn && isAdmin);
+console.log(isLoggedIn || isAdmin);
